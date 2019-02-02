83db6027e4557c530a7b995a86acce3867d2bca4
djbreen7 <daniel.breen@royaljay.net>
2019-02-01 18:32:51 -0700

    Use ngx-scrollbar

--- ToucanTesting.Client/ClientApp/src/app/app.module.ts
+++ ToucanTesting.Client/ClientApp/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgScrollbarModule } from 'ngx-scrollbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LayoutModule } from './layout/layout.module';
 import { ServicesModule } from './services/services.module';
 
+
 @NgModule({
   imports: [
     BrowserModule,
     LayoutModule,
     ServicesModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgScrollbarModule
   ],
   declarations: [
     AppComponent,
